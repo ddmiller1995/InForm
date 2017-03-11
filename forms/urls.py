49d9a3ad801f0f017e21fca2522f17c78537029b
Luis Naranjo <luisnaranjo733@gmail.com>
2017-03-11 11:05:30 -0800

    refactored to use class based views and html+json

--- forms/urls.py
+++ forms/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from forms import views
 
 urlpatterns = [
-    url(r'^forms/$', views.form_list),
-    url(r'^forms/(?P<pk>[0-9]+)/$', views.form_detail),
+    url(r'^forms/$', views.FormList.as_view()),
+    url(r'^forms/(?P<pk>[0-9]+)/$', views.FormDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- forms/views.py
+++ forms/views.py
@@ -1,52 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from forms.models import Form
 from forms.serializers import FormSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@csrf_exempt
-def form_list(request):
+class FormList(APIView):
     """
-    List all code forms, or create a new form.
+    List all forms, or create a new form.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         forms = Form.objects.all()
         serializer = FormSerializer(forms, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = FormSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = FormSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def form_detail(request, pk):
+class FormDetail(APIView):
     """
-    Retrieve, update or delete a code form.
+    Retrieve, update or delete a form instance.
     """
-    try:
-        form = Form.objects.get(pk=pk)
-    except Form.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Form.objects.get(pk=pk)
+        except Form.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        form = self.get_object(pk)
         serializer = FormSerializer(form)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = FormSerializer(form, data=data)
+    def put(self, request, pk, format=None):
+        form = self.get_object(pk)
+        serializer = FormSerializer(form, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        form = self.get_object(pk)
         form.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
