99ce5d9cce130300a5c4d967ecc0e0b849f50895
Luis Naranjo <luisnaranjo733@gmail.com>
2017-03-22 02:53:37 -0700

    succesfully configured nginx to serve django's static files!
    Set up a /static volume that I mounted on both containers.
    That way, you can run python manage.py collectstatic in the django
    container, and those files will be served from the /static directory
    in the nginx container.

--- src/InForm/settings.py
+++ src/InForm/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/static'
 
 STATICFILES_DIRS = (
     #This lets Django's collectstatic store our bundles
