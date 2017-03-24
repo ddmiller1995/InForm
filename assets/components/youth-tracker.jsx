a26df6bfdee86edca9a4fe3cac9d04a39050919c
Tessaev1 <tessaev1@uw.edu>
2017-03-24 12:31:21 -0700

    rendering MDL table with hardcoded data

--- assets/components/youth-tracker.jsx
+++ assets/components/youth-tracker.jsx
@@ -8,21 +8,32 @@ export default class YouthTracker extends React.Component {
         this.state = {};
     }
 
+    // table data is hard coded for now, but rows will likely become separate components rendered dynamically
     render() {
         return (
             <div className="youth-tracker-container">
-                <table className="youth-tracker">
+                <table className="mdl-data-table mdl-js-data-tabled">
                     <thead>
                         <tr>
-                            <th>Name</th>
+                            <th class="mdl-data-table__cell--non-numeric">Name</th>
                             <th>DOB</th>
+                            <th>Entry Date</th>
+                            <th>Intake Forms</th>
+                            <th>Outtake Forms</th>
+                            <th>Case Goals</th>
+                            <th>Planned Exit</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>John Smith</td>
+                            <td class="mdl-data-table__cell--non-numeric">John Smith</td>
                             <td>11/12/2001</td>
-                        </tr> 
+                            <td>2/13/2017</td>
+                            <td>Progress</td>
+                            <td>Progress</td>
+                            <td>Progress</td>
+                            <td>3/2/2017</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
