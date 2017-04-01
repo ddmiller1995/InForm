99a72def170b5c69ccd45dd239069b6630112204
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-01 14:22:01 -0700

    update admin to not use the previous User model

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Youth, YouthVisit, FormType, Form, FormYouthVisit, Task, TaskYouthVisit
+from .models import Youth, YouthVisit, FormType, Form, FormYouthVisit, Task, TaskYouthVisit
 
 models = [
-    User, Youth, YouthVisit,
+    Youth, YouthVisit,
     FormType, Form, FormYouthVisit,
     Task, TaskYouthVisit
 ]
