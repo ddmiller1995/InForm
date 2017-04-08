271163c46459f808abcfd8c1bddd0c77331373ef
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-07 20:34:29 -0700

    Implement the detail api view and clean up api view code
    Moved the serialization over to serializers.py to modularize code
    Implement a number of useful methods on the models
    
    Closes issue #28, closes issue #31

--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import timedelta, date
+from django.http import Http404
 
 class PlacementType(models.Model):
     placement_type_name = models.CharField(max_length=64, null=False, blank=False)
-    default_stay_length = models.IntegerField()
+    default_stay_length = models.IntegerField() # expressed as days
 
     def __str__(self):
         return self.placement_type_name
@@ -27,11 +28,42 @@ class Youth(models.Model):
     def __str__(self):
         return self.youth_name
 
+    def latest_youth_visit(self):
+        '''Return the latest youth_visit for this Youth
+        
+        Raise YouthVisit.DoesNotExist if this Youth does not have any YouthVisits'''
+        youth_visit = YouthVisit.objects.filter(youth_id=self) # filter by all YouthVisit rows that have FKs to this particular youth
+
+        if not youth_visit: # if no results found, skip this youth. This happens if a youth has no youth_visit
+            print(f'No youth visits found for pk={self.pk}!') # add this to logging? we really need a logger of some sort before deploying
+            raise YouthVisit.DoesNotExist
+
+        # now youth_visit is the specific youth_visit in the queryset with the latest 'placement_date'
+        youth_visit = youth_visit.latest('placement_date')
+        return youth_visit
+
     @staticmethod
-    def GetActiveYouth():
-        '''Return an iterable of activate youth objects'''
-        return []
-        return Youth.objects.all() # TODO: Replace placeholder code
+    def get_active_youth():
+        '''Return an iterable of activate youth objects
+        
+        * Estimated exit date = placement date + CURRENT placement type default stay duration
+        * Definition of active youth: "Youth whose current estimated exit date hasn't happened yet"
+        * Once a youth's estimated exit date has passed, we mark that youth different (maybe highlight in light red or something), but DON'T remove it from the list of active youth by default
+        * We add a mechanism for them to mark exit date
+        * If a youth has a exit date marked, only then do we remove them from the computed list of active youth
+        '''
+        active_youth = []
+        today = date.today()
+
+        for youth in Youth.objects.all():
+            try:
+                youth_visit = youth.latest_youth_visit()
+            except YouthVisit.DoesNotExist:
+                continue
+            if today <= youth_visit.estimated_exit_date():
+                active_youth.append(youth)
+
+        return active_youth
 
 
 class YouthVisit(models.Model):
@@ -66,6 +98,13 @@ class YouthVisit(models.Model):
     def __str__(self):
         return 'Youth: ' + self.youth_id.youth_name + ' - Placement date: ' + str(self.placement_date)
 
+    def estimated_exit_date(self):
+        '''Compute the current estimated exit date for this youth's visit
+        Estimated exit date = placement date + CURRENT placement type default stay duration
+        Returns a datetime.date object
+        '''
+        return self.placement_date + timedelta(days=self.placement_type.default_stay_length)
+        
 
 
 class FormType(models.Model):
