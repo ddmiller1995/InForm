ed7e23c63a223dac7c945aafc8a36aef0f8c8c29
Dakota Miller <ddmiller1995@gmail.com>
2017-04-07 17:53:24 -0700

    Combined the Form and Task entities into just Forms

--- src/api/models.py
+++ src/api/models.py
@@ -12,17 +12,17 @@ class PlacementType(models.Model):
 
 class School(models.Model):
     school_name = models.CharField(max_length=64, null=False, blank=False)
-    school_district = models.CharField(max_length=64)
-    school_phone = models.CharField(max_length=64)
+    school_district = models.CharField(max_length=64, null=True, blank=True)
+    school_phone = models.CharField(max_length=64, null=True, blank=True)
 
     def __str__(self):
         return self.school_name
 
 
 class Youth(models.Model):
-    youth_name = models.CharField(max_length=256, null=False, blank=False)
-    date_of_birth = models.DateField('date born', null=False, blank=False)
-    ethnicity = models.CharField(max_length=64)
+    youth_name = models.CharField(max_length=256)
+    date_of_birth = models.DateField('date born')
+    ethnicity = models.CharField(max_length=64, null=True, blank=True)
 
     def __str__(self):
         return self.youth_name
@@ -30,32 +30,45 @@ class Youth(models.Model):
     @staticmethod
     def GetActiveYouth():
         '''Return an iterable of activate youth objects'''
-        return []
         return Youth.objects.all() # TODO: Replace placeholder code
 
 
 class YouthVisit(models.Model):
     youth_id = models.ForeignKey(Youth, on_delete=models.CASCADE)
-    placement_date = models.DateField(
-        'placement date', null=False, blank=False)
-    city_of_origin = models.CharField(max_length=256)
-    guardian_name = models.CharField(max_length=256)
-    placement_type = models.ForeignKey(PlacementType, on_delete=models.SET_NULL, null=True)
-    referred_by = models.CharField(max_length=256)
-    permanent_housing = models.CharField(max_length=256)
-    exited_to = models.CharField(max_length=256)
+    placement_date = models.DateField('placement date')
+    city_of_origin = models.CharField(max_length=256, null=True, blank=True)
+    guardian_name = models.CharField(max_length=256, null=True, blank=True)
+    placement_type = models.ForeignKey(
+		PlacementType,
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True
+	)
+    referred_by = models.CharField(max_length=256, null=True, blank=True)
+    permanent_housing = models.BooleanField(default=False, null=True, blank=True)
+    exited_to = models.CharField(max_length=256, null=True, blank=True)
     case_manager = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, related_name='+')
+        User,
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True,
+		related_name='+'
+	)
     personal_counselor = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, related_name='+')
-    # School tracker fields
-    school = models.ForeignKey(School, on_delete=models.SET_NULL, null=True)
-    school_am_transport = models.CharField(max_length=256)
-    school_am_pickup_time = models.TimeField(null=True)
-    school_am_phone = models.CharField(max_length=64)
-    school_pm_transport = models.CharField(max_length=256)
-    school_pm_dropoff_time = models.TimeField(null=True)
-    school_pm_phone = models.CharField(max_length=64)
+        User,
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True,
+		related_name='+'
+	)
+    # School tracker fields - Not required fields
+    school = models.ForeignKey(School, on_delete=models.SET_NULL, null=True, blank=True)
+    school_am_transport = models.CharField(max_length=256, null=True, blank=True)
+    school_am_pickup_time = models.TimeField(null=True, blank=True)
+    school_am_phone = models.CharField(max_length=64, null=True, blank=True)
+    school_pm_transport = models.CharField(max_length=256, null=True, blank=True)
+    school_pm_dropoff_time = models.TimeField(null=True, blank=True)
+    school_pm_phone = models.CharField(max_length=64), null=True, blank=True
     # POSSIBLE COMPUTED FIELDS
     # YF enroll - Youthforce Enrollment Form submitted
     # YF exit - Youthforce Exit Form submitted
@@ -67,7 +80,6 @@ class YouthVisit(models.Model):
         return 'Youth: ' + self.youth_id.youth_name + ' - Placement date: ' + str(self.placement_date)
 
 
-
 class FormType(models.Model):
     form_type_name = models.CharField(max_length=256)
 
@@ -77,7 +89,12 @@ class FormType(models.Model):
 
 class Form(models.Model):
     form_name = models.CharField(max_length=256)
+	form_description = models.CharField(max_length=2048, null=True, blank=True)
     form_type_id = models.ForeignKey(FormType, on_delete=models.CASCADE)
+	# due date in days relative to entry date
+	# forms without due dates are allowed
+    default_due_date = models.IntegerField(null=True, blank=True)
+	# Form location - file location in static files?
 
     def __str__(self):
         return self.form_name
@@ -86,39 +103,9 @@ class Form(models.Model):
 class FormYouthVisit(models.Model):
     form_id = models.ForeignKey(Form, on_delete=models.CASCADE)
     youth_visit_id = models.ForeignKey(YouthVisit, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    completed = models.BooleanField(default=False)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+	# expected values: pending, in_progess, done
+    status = models.CharField(max_length=32, default='pending')
 
     def __str__(self):
         return 'Youth Visit ID: ' + self.youth_visit_id.id + ' - Form ID: ' + self.form_id.id
-
-
-class Task(models.Model):
-    task_name = models.CharField(max_length=256)
-    # due date in days relative to entry date
-    default_due_date = models.IntegerField(null=True, blank=True)
-    form_id = models.ForeignKey(
-        Form,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
-    form_type_id = models.ForeignKey(
-        FormType,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
-
-    def __str__(self):
-        return self.task_name
-
-
-class TaskYouthVisit(models.Model):
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
-    youth_visit_id = models.ForeignKey(YouthVisit, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return 'Youth Visit ID: ' + self.youth_visit_id.id + ' - Task ID: ' + self.task_id.id
