798fa4070c52cbff64ed400c940897f12302095b
Dakota Miller <ddmiller1995@gmail.com>
2017-04-10 18:21:43 -0700

    Added a method to the YouthVisit model to calcuate the progress through each form type

--- src/api/tests.py
+++ src/api/tests.py
@@ -9,30 +9,54 @@ from .models import *
 
 class YouthModelTests(TestCase):
 
-    def test_create_youth_succeeds(self):
-        youth = Youth(
+    def setUp(self):
+        youth1 = Youth.objects.create(
             youth_name="John",
             date_of_birth=datetime.date(1995, 12, 25),
             ethnicity="white"
         )
-        youth.save()
-        self.assertIsNotNone(youth.pk)
-
-    def test_create_visit_succeeds(self):
-        youth = Youth(
-            youth_name="John",
-            date_of_birth=datetime.date(1995, 12, 25),
-            ethnicity="white"
+        youth2 = Youth.objects.create(
+            youth_name="Bob",
+            date_of_birth=datetime.date(1999, 3, 7),
+            ethnicity="asian"
         )
-        youth.save()
-        youth_visit = YouthVisit(
-            youth_id=youth,
+        visit1 = YouthVisit.objects.create(
+            youth_id=youth1,
+            placement_date=datetime.date(2010, 1, 1),
+            city_of_origin="Bellingham"
+        )
+        visit2 = YouthVisit.objects.create(
+            youth_id=youth1,
             placement_date=timezone.localtime(timezone.now()).date(),
             city_of_origin="Seattle"
         )
-        youth_visit.save()
-        self.assertIsNotNone(youth_visit.pk)
-        youth.delete()
-        self.assertIsNone(youth.pk)
-        print(youth_visit.pk)
-        print(youth_visit.youth_id)
+        visit3 = YouthVisit.objects.create(
+            youth_id=youth2,
+            placement_date=timezone.localtime(timezone.now()).date() - timedelta(days=3),
+            city_of_origin="Seattle"
+        )
+        form_type1 = FormType.objects.create(form_type_name="Intake")
+        form_type2 = FormType.objects.create(form_type_name="Outtake")
+        form1 = Form.objects.create(form_name="Form 1", form_type_id=form_type1)
+        form2 = Form.objects.create(form_name="Form 2", form_type_id=form_type1)
+        form3 = Form.objects.create(form_name="Form 3", form_type_id=form_type2)
+        form_youth_visit1 = FormYouthVisit.objects.create(form_id=form1, youth_visit_id=visit2, status='done')
+        #form_youth_visit2 = FormYouthVisit.objects.create(form_id=form1, youth_visit_id=visit2, status='done')
+        #form_youth_visit3 = FormYouthVisit.objects.create(form_id=form1, youth_visit_id=visit2, status='done')
+
+
+
+    def test_create_youth_succeeds(self):
+        self.assertIsNotNone(Youth.objects.all()[0].pk)
+
+    def test_create_visit_succeeds(self):
+        self.assertIsNotNone(YouthVisit.objects.all()[0].pk)
+
+    # TODO: Expand testing for this method and for the other model methods
+    def test_form_type_progress_method(self):
+        visit = YouthVisit.objects.filter(pk=2)[0]
+        progress = visit.form_type_progress()
+        self.assertIsNotNone(progress)
+        self.assertEqual(progress["Intake"], 0.5)
+        self.assertEqual(progress["Outtake"], 0.0)
+
