de596065bae74b3c79bc21ea93486568a2761e1b
Dakota Miller <ddmiller1995@gmail.com>
2017-05-03 23:21:53 -0700

    Began adding support for grabbing form data from API

--- src/assets/components/youth-progress.jsx
+++ src/assets/components/youth-progress.jsx
@@ -18,73 +18,83 @@ export default class extends React.Component {
     componentDidMount() {
         let response = getRequest("/api/form-type", this, "form_types");
 
-        form_data.push({
-            form_name: "form A",
-            form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, perspiciatis quasi tenetur, ea sed, molestiae illo labore voluptatem fugiat delectus illum culpa dolore architecto saepe doloribus aliquid reprehenderit autem at!",
-            default_due_date: "10",
-            required: "false", 
-            user_id: {
-                username: "dakota",
-                full_name: "dakota miller"
-            },
-            days_remaining: "3",
-            status: "pending",
-            form_type: "Intake"
-        });
-        form_data.push({
-            form_name: "form B",
-            form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. ",
-            default_due_date: "10",
-            required: "false", 
-            user_id: {
-                username: "dakota",
-                full_name: "dakota miller"
-            },
-            days_remaining: "0",
-            status: "pending",
-            form_type: "Intake"
-        });
-        form_data.push({
-            form_name: "form C",
-            form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, perspiciatis quasi tenetur, ea sed, molestiae illo labore voluptatem fugiat delectus illum culpa dolore architecto saepe doloribus aliquid reprehenderit autem at!",
-            default_due_date: "10",
-            required: "false", 
-            user_id: {
-                username: "dakota",
-                full_name: "dakota miller"
-            },
-            days_remaining: "3",
-            status: "pending",
-            form_type: "Psych"
-        });
-        form_data.push({
-            form_name: "form D",
-            form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, perspiciatis quasi tenetur, ea sed, molestiae illo labore voluptatem fugiat delectus illum culpa dolore architecto saepe doloribus aliquid reprehenderit autem at!",
-            default_due_date: "10",
-            required: "false", 
-            user_id: {
-                username: "dakota",
-                full_name: "dakota miller"
-            },
-            days_remaining: "0",
-            status: "in progress",
-            form_type: "Intake"
-        });
-        form_data.push({
-            form_name: "form E",
-            form_description: "Lorem ipsum",
-            default_due_date: "10",
-            required: "false", 
-            user_id: {
-                username: "dakota",
-                full_name: "dakota miller"
-            },
-            days_remaining: "3",
-            status: "done",
-            form_type: "Outtake"
-        });
+        // form_data.push({
+        //     form_name: "form A",
+        //     form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, perspiciatis quasi tenetur, ea sed, molestiae illo labore voluptatem fugiat delectus illum culpa dolore architecto saepe doloribus aliquid reprehenderit autem at!",
+        //     default_due_date: "10",
+        //     required: "false", 
+        //     user_id: {
+        //         username: "dakota",
+        //         full_name: "dakota miller"
+        //     },
+        //     days_remaining: "3",
+        //     status: "pending",
+        //     form_type: "Intake"
+        // });
+        // form_data.push({
+        //     form_name: "form B",
+        //     form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. ",
+        //     default_due_date: "10",
+        //     required: "false", 
+        //     user_id: {
+        //         username: "dakota",
+        //         full_name: "dakota miller"
+        //     },
+        //     days_remaining: "0",
+        //     status: "pending",
+        //     form_type: "Intake"
+        // });
+        // form_data.push({
+        //     form_name: "form C",
+        //     form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, perspiciatis quasi tenetur, ea sed, molestiae illo labore voluptatem fugiat delectus illum culpa dolore architecto saepe doloribus aliquid reprehenderit autem at!",
+        //     default_due_date: "10",
+        //     required: "false", 
+        //     user_id: {
+        //         username: "dakota",
+        //         full_name: "dakota miller"
+        //     },
+        //     days_remaining: "3",
+        //     status: "pending",
+        //     form_type: "Psych"
+        // });
+        // form_data.push({
+        //     form_name: "form D",
+        //     form_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, perspiciatis quasi tenetur, ea sed, molestiae illo labore voluptatem fugiat delectus illum culpa dolore architecto saepe doloribus aliquid reprehenderit autem at!",
+        //     default_due_date: "10",
+        //     required: "false", 
+        //     user_id: {
+        //         username: "dakota",
+        //         full_name: "dakota miller"
+        //     },
+        //     days_remaining: "0",
+        //     status: "in progress",
+        //     form_type: "Intake"
+        // });
+        // form_data.push({
+        //     form_name: "form E",
+        //     form_description: "Lorem ipsum",
+        //     default_due_date: "10",
+        //     required: "false", 
+        //     user_id: {
+        //         username: "dakota",
+        //         full_name: "dakota miller"
+        //     },
+        //     days_remaining: "3",
+        //     status: "done",
+        //     form_type: "Outtake"
+        // });
     }
 
+
+    componentWillReceiveProps() {
+        // try {
+        //     form_data = this.props.currentYouth.youth_visits[0].forms;
+        //     forceUpdate();
+        // } catch(TypeError) {
+        //     form_data = [];
+        // }
+        //forceUpdate();
+    }
 
     getColumns() {
         let columns = [];
@@ -105,6 +115,11 @@ export default class extends React.Component {
       
     render() {
         let columns = this.getColumns();
+        if(this.props.currentYouth.youth_visits) {
+            console.log("yes");
+            form_data = this.props.currentYouth.youth_visits[0].forms;
+            console.log(this.props.currentYouth.youth_visits);
+        }
 
         return (
             <div className="container">
