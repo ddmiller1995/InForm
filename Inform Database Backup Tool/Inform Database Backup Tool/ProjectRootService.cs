3e6ab756d1376f2ef2922ef409114dc75e6abbaa
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-16 10:53:21 -0700

    command service is working

---
+++ Inform Database Backup Tool/Inform Database Backup Tool/ProjectRootService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupApi
+{
+    public class ProjectRootService
+    {
+        public static string ComputeProjectRoot()
+        {
+            return @"C:\code\InForm\src";
+        }
+    }
+}
---
+++ Inform Database Backup Tool/Inform Database Backup Tool/SaveFileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inform_Database_Backup_Tool
+{
+    public class SaveFileService
+    {
+        public static bool SaveFile(string filename, string body)
+        {
+            return true;
+        }
+    }
+}
