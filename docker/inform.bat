c1b775f2434ce16449547749ac8eaf08b9b8434c
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-26 10:47:07 -0700

    update auto restart batch script to use timeout isntead of sleep

--- docker/inform.bat
+++ docker/inform.bat
@@ -8,13 +8,13 @@ set /A docker_off = "-1"
 
 If %docker_running% == %docker_off% (
     echo "Docker is not running yet. Sleeping for 2 seconds and then checking again"
-    SLEEP 2
+    timeout 2
     GOTO wait_for_docker
 )
 
 echo "RESTARTING DOCKER"
 cd C:\code\InForm
-SLEEP 30
+timeout 30
 docker-compose down
 docker-compose up -d
 PAUSE
